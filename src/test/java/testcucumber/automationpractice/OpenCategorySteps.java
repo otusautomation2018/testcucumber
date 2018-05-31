9ece7b05aec2cabcad244668674e56940698b93b
Anton Karpenko <toshidono.it.work@gmail.com>
2018-05-31 07:09:49 +0300

    add hooks

--- src/test/java/testcucumber/automationpractice/OpenCategorySteps.java
+++ src/test/java/testcucumber/automationpractice/OpenCategorySteps.java
@@ -4,52 +4,33 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.automationpractice.MainPage;
-import utils.Driver;
-import utils.PropertyReader;
 import utils.helpers.ActionsHelpers;
 import utils.helpers.WaitingsHelpers;
 
-import java.util.concurrent.TimeUnit;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static testcucumber.BaseSteps.getDriver;
+import static testcucumber.BaseSteps.getWebDriverWait;
 
 public class OpenCategorySteps {
-
-    public int baseTimeout = Integer.parseInt(PropertyReader.
-            getPropertyFromFile("properties/settings.properties", "timeout"));
-
-    protected WebDriver driver;
-    protected WebDriverWait webDriverWait;
-
-    public void prepare(){
-        driver = Driver.getDriver();
-        driver.manage().timeouts().implicitlyWait(baseTimeout, TimeUnit.SECONDS);
-        webDriverWait = WaitingsHelpers.getInstanceWebDriverWait(driver);
-    }
 
     private MainPage mainPage;
     private final String T_SHIRTS_PAGE_TITLE = "T-shirts - My Store";
 
     @Given("^I am on the Automationpractice Main page$")
     public void i_am_on_the_automationpractice_main_page() {
-        prepare();
-        mainPage = new MainPage(driver);
-        driver.get(mainPage.baseUrl);
-        WaitingsHelpers.waitForLoadPageByTextOnPage(driver, mainPage.h1OfexpectedText, mainPage.expectedTextOnPage());
-        assertTrue("Current Url not equals expected Url.", driver.getCurrentUrl().contains(mainPage.getUrl()));
+        mainPage = new MainPage(getDriver());
+        getDriver().get(mainPage.baseUrl);
+        WaitingsHelpers.waitForLoadPageByTextOnPage(getDriver(), mainPage.h1OfexpectedText, mainPage.expectedTextOnPage());
+        assertTrue("Current Url not equals expected Url.", getDriver().getCurrentUrl().contains(mainPage.getUrl()));
         assertTrue(mainPage.getClass() + " is not initialized. ", mainPage.isInitialized());
-
-
     }
 
     @When("^I focus on Women category$")
     public void i_focus_on_women_category() {
-        ActionsHelpers.focusOnElement(driver, mainPage.topMenuBlock.firstCategory, mainPage.topMenuBlock.subMenu);
+        ActionsHelpers.focusOnElement(getDriver(), mainPage.topMenuBlock.firstCategory, mainPage.topMenuBlock.subMenu);
     }
 
     @And("^I click T-shirts link$")
@@ -59,12 +40,7 @@ public class OpenCategorySteps {
 
     @Then("^I see T-shirts page$")
     public void i_see_t_shirts_page() {
-        webDriverWait.until(ExpectedConditions.titleIs(T_SHIRTS_PAGE_TITLE));
-        assertEquals("TShirts Page title is not displayed.",driver.getTitle(), T_SHIRTS_PAGE_TITLE);
-        quit();
-    }
-
-    public void quit() {
-        driver.quit();
+        getWebDriverWait().until(ExpectedConditions.titleIs(T_SHIRTS_PAGE_TITLE));
+        assertEquals("TShirts Page title is not displayed.",getDriver().getTitle(), T_SHIRTS_PAGE_TITLE);
     }
 }
--- src/test/java/testcucumber/google/GoogleSteps.java
+++ src/test/java/testcucumber/google/GoogleSteps.java
@@ -3,46 +3,36 @@ package testcucumber.google;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import utils.Driver;
 
-import java.util.concurrent.TimeUnit;
+import static testcucumber.BaseSteps.getDriver;
+import static testcucumber.BaseSteps.getWebDriverWait;
 
 public class GoogleSteps {
 
-    private WebDriver driver;
     private final String BASE_URL = "https://google.com";
-    private final int BASE_TIMEOUT = 10;
     private final static By QUERY_NAME = By.name("q");
     private final static By RESULT_STATS_CSS = By.cssSelector("div[id=resultStats");
 
     @Given("^I am on the Google search page$")
     public void i_am_on_the_google_search_page() {
-        WebDriverManager.chromedriver().setup();
-        driver = Driver.getDriver();
-        driver.manage().timeouts().implicitlyWait(BASE_TIMEOUT, TimeUnit.SECONDS);
-
-        driver.get(BASE_URL);
+        getDriver().get(BASE_URL);
     }
 
     @When("^I search for \"(.*)\"$")
     public void i_search_for(String query) {
-        WebElement searchField = driver.findElement(QUERY_NAME);
+        WebElement searchField = getDriver().findElement(QUERY_NAME);
         searchField.sendKeys(query);
         searchField.submit();
     }
 
     @Then("The page title contains \"(.*)\"$")
     public void the_page_title_contains(String query) {
-        WebElement element = driver.findElement(RESULT_STATS_CSS);
-        new WebDriverWait(driver, BASE_TIMEOUT).until(ExpectedConditions.visibilityOf(element));
-        Assert.assertTrue(driver.getTitle().contains(query));
-        driver.quit();
+        WebElement element = getDriver().findElement(RESULT_STATS_CSS);
+        getWebDriverWait().until(ExpectedConditions.visibilityOf(element));
+        Assert.assertTrue(getDriver().getTitle().contains(query));
     }
 }
